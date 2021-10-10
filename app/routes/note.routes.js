dac1a5c720351ff64c07c1d419a8c6bf2da9e95a
zuwannn <suwanneethongtha@gmail.com>
2021-10-10 22:56:26 +0700

    Defining Routes using Express

---
+++ app/routes/note.routes.js
@@ -0,0 +1,15 @@
+module.exports = (app) => {
+    const notes = require('../controllers/note.controller')
+
+    // create a new note
+    app.post('/notes', notes.create)
+
+    // retrieve all note
+    app.get('/notes', notes.findAll)
+
+    // update a note with noteId
+    app.put('/notes/:noteId', notes.update)
+
+    // delete a note with noteId
+    app.delete('/notes/:noteId', notes.delete)
+}
