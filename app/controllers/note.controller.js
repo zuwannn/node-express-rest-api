f9b3e575cc0106431eb2ace1542605b884a82760
zuwannn <suwanneethongtha@gmail.com>
2021-10-10 23:06:22 +0700

    Writing the Controller functions

---
+++ app/controllers/note.controller.js
@@ -0,0 +1,26 @@
+const Note = require('../models/note.model')
+
+// create and save a new Note
+exports.create = (req, res) => {
+    
+}
+
+// retrieve and return all notes from the database
+exports.findAll = (req, res) => {
+    
+}
+
+// find a single note with a noteId
+exports.findOne = (req, res) => {
+
+}
+
+// update a note identified by the noteId in the request
+exports.update = (req, res) => {
+
+}
+
+// delete a note with the specified noteId in the request
+exports.delete = (req, res) => {
+
+}
