3f04fa751c77cea03a7a6947693a382ccc9a39c5
zuwannn <suwanneethongtha@gmail.com>
2021-10-10 23:38:50 +0700

    Retrieving a single Note

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -41,7 +41,24 @@ exports.findAll = (req, res) => {
 
 // find a single note with a noteId
 exports.findOne = (req, res) => {
-
+    Note.findById(req.params.noteId)
+    .then(note => {
+        if(!note){
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            })
+        }
+        res.send(note)
+    }).catch(err => {
+        if(err.kind === 'ObjectId'){
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            })
+        }
+        return res.status(500).send({
+            message: "Error retrieving note with id " + req.params.noteId
+        })
+    })
 }
 
 // update a note identified by the noteId in the request
