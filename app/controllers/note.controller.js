e735a9f77749314b2ffb765d6961847e0447d6e3
zuwannn <suwanneethongtha@gmail.com>
2021-10-10 23:13:18 +0700

    Creating a new Note

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -2,7 +2,29 @@ const Note = require('../models/note.model')
 
 // create and save a new Note
 exports.create = (req, res) => {
-    
+
+    // validate request
+    if(!req.body.content){
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        })
+    }
+
+    // create a note
+    const note = new Note({
+        title: req.body.title || "Untitled Note",
+        content: req.body.content
+    })
+
+    // save Note in the database
+    note.save()
+    .then(data => {
+        res.send(data)
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the Note."
+        })
+    })
 }
 
 // retrieve and return all notes from the database
