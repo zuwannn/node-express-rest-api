3634f832aa4e4ef14c310d66cc18a9f5038fc904
zuwannn <suwanneethongtha@gmail.com>
2021-10-11 00:00:23 +0700

    Updating a Note

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -63,7 +63,37 @@ exports.findOne = (req, res) => {
 
 // update a note identified by the noteId in the request
 exports.update = (req, res) => {
+    // validate request
+    if(!req.body.content){
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        })
+    }
 
+    // find note and update it with the request body
+    Note.findByIdAndUpdate(req.params.noteId, {
+        title: req.body.title || "Untitled Note",
+        content: req.body.content
+    },{
+        new: true
+    })
+    .then(note => {
+        if(!note){
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            })
+        }
+        res.send(node)
+    }).catch(err => {
+        if(err.kind === 'ObjectId'){
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            })
+        }
+        return res.status(500).send({
+            message: "Error updating note with id " + req.params.noteId
+        })
+    })
 }
 
 // delete a note with the specified noteId in the request
